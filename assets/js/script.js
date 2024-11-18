f12f365687dcf3fe4a6c451b24a398561bbf1ccf
manuelcoder02 <sotundeemmanuel@gmail.com>
2024-11-18 23:05:58 +0100

    finished the light and dark theme script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,35 @@
-'use strict;'
-import {API_URL} from './config.js'
-import { getJSON } from './helpers.js'
+"use strict;";
+import { API_URL } from "./config.js";
+import { getJSON } from "./helpers.js";
 
 // LOAD API
-const loadAPI = async function() {
-    try {
-        const data = await getJSON(`${API_URL}`)
-        console.log(data);
-    } catch (error) {
-        console.error(error.message);
-    }
-}
+const loadAPI = async function () {
+  try {
+    const data = await getJSON(`${API_URL}`);
+    console.log(data);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
-loadAPI()
+loadAPI();
+
+const sModeEL = document.querySelector(".switch__mode");
+const containerBody = document.querySelector("body");
+const questionTagEL = document.querySelector(".question__tag");
+const quizMenuEL = document.querySelector(".quiz__menu");
+
+!quizMenuEL.classList.contains("hidden")
+  ? questionTagEL.classList.add("visibility-hidden")
+  : questionTagEL.classList.remove("visibility-hidden");
 
 // TOGGLE SWITCH MODE FUNCTIONALITY
-const sModeEL = document.querySelector('.switch__mode');
-const containerBody = document.querySelector('body')
-const iconDarkMode = document.querySelector('.dark-mode')
-const iconLightMode = document.querySelector('.light-mode')
-const svgDM = document.querySelector('#dm')
-const svgLM = document.querySelector('#lm')
-const whiteSwitch = document.querySelector('.switch')
- const toggleSwitch = function() {
-    sModeEL.addEventListener('click', function(){
-        containerBody.classList.toggle('dark__mode')
-        iconDarkMode.classList.toggle('icon__dark-mode')
-        iconLightMode.classList.toggle('icon__light-mode')
-        svgDM.classList.toggle('icon__dm')
-        svgLM.classList.toggle('icon__lm')
-        whiteSwitch.classList.toggle('switch__two')
-    })
- }
+const toggleSwitch = () => {
+  sModeEL.addEventListener("click", () => {
+    // body.classList.toggle('dark-theme')
+    containerBody.classList.toggle("dark__mode");
+    containerBody.classList.toggle("dark-theme");
+  });
+};
 
- toggleSwitch()
+toggleSwitch();
